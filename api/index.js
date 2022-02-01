275c138da32057594676ecb0755192f82480bf3e
Juan Huarte <juan_huarte@hotmail.com>
2022-02-01 12:45:38 -0300

    Agrego cambios para heroku

--- api/index.js
+++ api/index.js
@@ -23,7 +23,7 @@ const { conn } = require("./src/db.js");
 const { getGenres } = require("./src/functionHelpers/functions");
 
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: false }).then(() => {
   server.listen(process.env.PORT, async () => {
     console.log("%s listening at " + process.env.PORT); // eslint-disable-line no-console
     // aca tendria que hacer el precargado de los genres
